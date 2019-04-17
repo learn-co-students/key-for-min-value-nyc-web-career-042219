f602d9d452253c5c96b99ecbfd4d325854fd6d77
Jessica Lin <jessicaa.lin@gmail.com>
2019-04-17 10:33:28 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_name = nil
+  min_name = nil #var assignments to start out of the map scope
   min_num = nil
-  name_hash.collect do |name, num|
+  name_hash.map do |name, num|
     if min_num == nil || num < min_num # if hash is empty to return nil
       min_num = num
       min_name = name
